0898b9553506689c1dce0111eef8b3b5048ea216
BenjaminAJ <ajanakub30@gmail.com>
2023-10-04 12:57:20 +0100

    Optimize Todo component, Redux for state and form.

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import TodoList from './TodoList'
-import { useDispatch } from 'react-redux'
-import { addtodo } from '../../redux/ReduxRequest/Todo'
+import { useDispatch, useSelector } from 'react-redux'
+import { addtodo, editTodo } from '../../redux/ReduxRequest/Todo'
 import {  useFormik } from 'formik'
 import todoSchema from '../../Schema'
+import UpdateTodo from './UpdateTodo'
+import AddTodo from './AddTodo'
 
 const Todo = () => {
   const [loading, setloading] = useState(false)
+  const [editing, setediting] = useState(false)
 
   const dispatch = useDispatch();
 
@@ -31,7 +34,23 @@ const Todo = () => {
       setloading(false)
     }, 500);
   }
-  return (
+  const {todos} = useSelector(state=> state.todo)
+
+  const handleEdiTodo = (e) => {
+    e.preventDefault();
+    let index = values.index
+    if (index) {
+      console.log('todo not found');
+      return  
+    }
+      setloading(true);
+      dispatch(editTodo({ values: values.title, index }));
+      setTimeout(() => {
+        setloading(false);
+        setediting(false);
+      }, 1000);
+  };
+    return (
     <div>
       <section className="vh-100">
         <div className="container py-5 h-100">
@@ -48,13 +67,13 @@ const Todo = () => {
                   <div className="pb-2">
                     <div className="card">
                       <div className="card-body">
-                        <form onSubmit={handleSubmit} className="d-flex flex-column flex-md-row  align-items-center">
+                        <form onSubmit={editing ? handleEdiTodo : handleSubmit } className="d-flex flex-column flex-md-row  align-items-center">
                           <input onChange={handleChange} value={values.title} onBlur={handleBlur} name='title' type="text" className="form-control form-control-lg" id="exampleFormControlInput1"
                             placeholder="Add new..." />
                             <p className='text-danger'>{errors.title && touched.title ? '*Required' : ''}</p>
                           <Link to="#!" data-mdb-toggle="tooltip" title="Set due date"><i className="bi bi-calendar ms-1 me-1"></i></Link>
                           <div>
-                            <button disabled={loading}   type="submit" className="btn btn-primary">{loading ? 'Loading...' : 'Add'}</button>
+                            {editing ? <UpdateTodo loading={loading}/> : <AddTodo loading={loading}/>}
                           </div>
                         </form>
                       </div>
@@ -62,7 +81,7 @@ const Todo = () => {
                   </div>
 
                   <hr className="my-4" />
-                  <TodoList/>
+                  <TodoList editing={editing} setediting={setediting} values={values}/>
 
                 </div>
               </div>
