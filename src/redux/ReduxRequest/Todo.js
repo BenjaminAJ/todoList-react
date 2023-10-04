9463ac1d35683e5f8b5bcc7d0be305c354beb68e
BenjaminAJ <ajanakub30@gmail.com>
2023-10-04 12:57:57 +0100

    Refactor TodoSlice: add, update, delete, and edit functionality.

--- src/redux/ReduxRequest/Todo.js
+++ src/redux/ReduxRequest/Todo.js
@@ -20,10 +20,23 @@ export const TodoSlice = createSlice({
         },
         deleteTodo:(state, action)=>{
             state.todos = state.todos.filter((todo)=> {return todo.title != action.payload})
-        }
-    }
+        }, 
+        editTodo: (state, action) => {
+            const { index, values } = action.payload;
+          
+            const updatedTodos = state.todos.map((todo, i) =>
+              i === index ? { ...todo, title: values } : todo
+            );
+          
+            // Return a new state object
+            return {
+              ...state,
+              todos: updatedTodos,
+            };
+            }
+            }
 })
 
-export const {addtodo, updateChecked, deleteTodo} = TodoSlice.actions;
+export const {addtodo, updateChecked, deleteTodo, editTodo} = TodoSlice.actions;
 
 export default TodoSlice.reducer;
